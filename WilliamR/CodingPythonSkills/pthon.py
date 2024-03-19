a0c99926262a6c459ef951e439084f4e84fbc19c
SkillSamuraiAshmore <skillsamuraiashmore@gmail.com>
2024-03-19 15:06:31 +1000

    Test stuff

---
+++ WilliamR/CodingPythonSkills/pthon.py
@@ -0,0 +1,17 @@
+print("hello world")
+print("hi my name is will")
+print("i am will")
+age = 10
+print(age)
+print("i am " + str (age))
+print("i am " + str (age) + "years old")
+print (5 + 5)
+print(age + 10000)
+print(age / 2)
+print(100 - 100)
+print(7 * 7)
+name = input("what is your name")
+fruit = "apple"
+Amount = 10
+price = 500.22
+delicious = true
