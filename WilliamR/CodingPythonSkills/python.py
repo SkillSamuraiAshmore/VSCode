6b1de72c416b60fda1d242960a9fab5ce72982fb
SkillSamuraiAshmore <skillsamuraiashmore@gmail.com>
2024-04-16 14:40:05 +1000

    yep

--- WilliamR/CodingPythonSkills/python.py
+++ WilliamR/CodingPythonSkills/python.py
@@ -1,8 +1,26 @@
 score = 90
+coupon =1
 if(score < 100):
     print ("bye")
-money = 0
-if money >= 10:
+money = 1
+if money >= 100:
     print("food")
-if money >= 5:
-    print("large fries from mcdonalds")
+elif money >= 5 or coupon:
+    print("snack")
+else:
+    print("Nothing")
+hungry = False
+coupon = True
+fruit_list = ["apple", "bannana" ,"cherry"]
+print(fruit_list)
+print(fruit_list[0])
+fruit_list.append("apple")
+fruit_list[2] = "strawberry"
+print(fruit_list)
+snack_list = ["chocolate","candy","ice cream"]
+shopping_list = fruit_list + snack_list
+print(shopping_list)
+shopping_list.remove("bannana")
+print(shopping_list)
+range(10000000000000,200000000000000000000)
+list(range(200000000000000000000))
