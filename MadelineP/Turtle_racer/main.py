3326ad24076ee574b929b040065312bf76981e89
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-05-03 16:21:30 +1000

    made screen

---
+++ MadelineP/Turtle_racer/main.py
@@ -0,0 +1,11 @@
+import time
+import turtle
+from turtle import *
+from random import *
+
+window = turtle.Screen()
+window.tittle("Turtle Racer")
+turtle.bgcolor("lightgreen")
+
+while True:
+    window
