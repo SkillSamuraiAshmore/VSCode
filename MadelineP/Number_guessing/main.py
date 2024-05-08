1af00558eeaa6b40af491ef1d0e05e19b727b113
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-05-08 16:10:18 +1000

    added question promt

---
+++ MadelineP/Number_guessing/main.py
@@ -0,0 +1,16 @@
+import random
+
+score = ["LeaderBoard"]
+
+def game():
+    print('')
+    difficulty = (input("Please choose a difficulty 1,2 or 3"))
+
+    if difficulty == '1':
+        maxNumber = 10
+    
+    elif difficulty == '2':
+        maxNumber = 20
+    
+    elif difficulty == '3':
+        maxNumber = 30
