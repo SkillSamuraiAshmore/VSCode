3d23c638477dab60a8dcf7c919834bd6318552d0
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-05-08 16:25:21 +1000

    added guess counter

--- MadelineP/Number_guessing/main.py
+++ MadelineP/Number_guessing/main.py
@@ -21,4 +21,23 @@ def game():
     my_name = input("what is your name? ")
     print("Well, " + my_name + " , I am thinking of a number between 1 and " + str(maxNumber))
 
+    for guesstaken in range(lives):
+        print("You have " + str(lives - guesstaken) + "guesses left")
+        guess = input('Take a guess: ')
+        guess = int(guess)
+
+        if guess == number:
+            print("good job! You won!")
+        
+        elif guess < number:
+            print("Your guess is too low")
+
+        elif guess > number:
+            print("Your guess is too high")
+
+        elif guess > maxNumber:
+            print("Your guess is out of range")
+
+        
+
 game()
