132e8188c56b589013651d52c9328998626b4ec8
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-05-22 16:36:02 +1000

    put text onto screen

--- MadelineP/pong/main.py
+++ MadelineP/pong/main.py
@@ -32,6 +32,16 @@ ball.goto(0, 0)
 ball.dx = 0.12
 ball.dy = 0.12
 
+#pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -79,6 +89,16 @@ while True:
 
     if ball.xcor() < -390:
         ball.goto(0,0)
+        ball.dx *= -1
+
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-
+                  340)
         ball.dx *= -1
 
    
