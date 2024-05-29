77cb93b98c8758555c83979296c54e4e8151f471
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-05-29 16:12:33 +1000

    created basic game loop

--- MadelineP/Wordle.py/play_wordle.py
+++ MadelineP/Wordle.py/play_wordle.py
@@ -1,5 +1,18 @@
+from wordle import Wordle
+
 def main():
     print("hello world!")
+    wordle = Wordle("APPLE")
+
+    while wordle.can_attempt:
+        x = input("Type your guess: ")
+        wordle.attempt(x)
+        
+    if wordle.is_solved:
+        print("You've sovled the puzzle")
+
+    else:print("You have failed to solve the puzzle!")
 
 if __name__ == "__main__":
     main()
+
---
+++ MadelineP/Wordle.py/wordle.py
@@ -0,0 +1,26 @@
+class Wordle():
+
+    MAX_ATTEMPTS = 6 
+    WORD_LENGTH = 5
+
+    def __init__(self, secret: str):
+        self.secret: str = secret
+        self.attempts = []
+        pass
+
+    def attempt(self, word: str):
+        self.attempts.append(word)
+
+    
+    @property   
+    def is_solved(self):
+        return len(self.attempts) > 0 and self.attempts[-1] == self.secret
+    
+    @property
+    def remainin_attempts(self):
+        return self.MAX_ATTEMPTS - len(self.attempts)
+    
+    @property
+    def can_attempt(self):
+        return self.remainin_attempts > 0 and not self.is_solved
+    
