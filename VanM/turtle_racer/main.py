4e424223ac92f2a16b94f2cd4339813921c09ad0
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-06-11 16:19:17 +1000

    i did turtle 1

--- VanM/turtle_racer/main.py
+++ VanM/turtle_racer/main.py
@@ -7,6 +7,10 @@ window = turtle.Screen()
 window.title("Turtle Racer 1.0")
 turtle.bgcolor("lightgreen")
 
+stamp_size = 20
+square_size = 15
+finsh_line = 200
+
 t = turtle.Pen()
 t.speed(0)
 t.penup()
@@ -26,6 +30,28 @@ for n in range (2):
     t.forward(300)
     t.right(90)
 t.end_fill()
+
+t.color("black")
+t.shape("square")
+t.shapesize(square_size / stamp_size)
+t.penup()
+for i in range (10):
+    t.setpos(finsh_line, (150 - (i * square_size * 2)))
+    t.stamp()
+for j in range (10):
+    t.setpos((finsh_line + square_size), (150 - square_size) - (j * square_size * 2))
+    t.stamp()
+    
+t1 = Turtle()
+t1.speed(0)
+t1.color("magenta") 
+t1.shape("turtle")
+t1.penup()
+t1.goto(-250, 100)
+
+
+
+
 
 
 turtle.done()
