1ed3915d10fb78fba34b81ddeacb1e33b13616e1
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-06-12 17:44:53 +1000

    finished basics

--- JoshH/fundamentals/main.py
+++ JoshH/fundamentals/main.py
@@ -1,11 +1,19 @@
-print("hello world")
-print("hello my name is peter")
-print("twinkle, twinkle, litle star, how i wonder what you are!, up above the world so high, like a diamond in the sky,twinkle twinkle little star, how i wonder what you are")
-age = 20
-print(age)
-print("my age is " + str(age)+ " years old")
-light = input("what is the traffic light")
-if light == " red":
-    print("stop")
+#  green
+# age = 20
+# print(age)
+# print("my age is " + str(age)+ " years old")
+# light = input("what is the traffic light")
+# if light == " red":
+#     print("stop")
     
-    
+# print("my age is " + str(age) + " years old")
+# age = age+1
+
+# light = input("what is the traffic light")
+# if light == " red":
+#     print("stop")
+# elif light == " green":
+#     print("go")
+# else:
+#     print("uh oh ")
+
