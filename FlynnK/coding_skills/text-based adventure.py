1916f966150efaef35b27a393535491402be454d
SkillSamuraiAshmore <skillsamuraiashmore@gmail.com>
2024-06-19 15:14:20 +1000

    dzcd

--- FlynnK/coding_skills/text-based adventure.py
+++ FlynnK/coding_skills/text-based adventure.py
@@ -26,9 +26,23 @@ def damage(damage, hp):
         
 typewriter("hello")
 
+def way():
+    typewriter("You are in a cave. There are tow ways you can go, left and right. Which way? [left or right]")
+    decision = input()
+    
+    if decision == "left":
+        typewriter("The left path leads you to a small cavern with a spring in the centre and a key on top of the spring")
+        typewriter("The key must lead to the samll rotted door to the right of the spring.")
+    elif decision == "right":
+        typewriter("You walk down the right path, but run into some spikes.")
+        damage(random.randint(1, 3), hp)
+
 def start_game():
     typewriter("begin the game? [yes or no]")
     play = input()
     
     if play == "yes":
         typewriter("let's begin!")
+        
+start_game()
+        
