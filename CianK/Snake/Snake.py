da7e8353bf82ef89c820b0c704e72c1cf6ffa14b
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-07-08 17:07:40 +1000

    cian-game

--- CianK/Snake/Snake.py
+++ CianK/Snake/Snake.py
@@ -2,10 +2,10 @@ import turtle
 import random
 import time
 
-snake_screen = turtle.Screen()
-snake_screen.title("Snake")
-snake_screen.setup(width=600, height=600)
-snake_screen.tracer(0)
+snakeScreen = turtle.Screen()
+snakeScreen.title("Snake")
+snakeScreen.setup(width = 600, height = 600)
+snakeScreen.tracer(0)
 
 snakeHead = turtle.Turtle()
 snakeHead.shape("square")
@@ -17,15 +17,16 @@ delay = 0.2
 segments = []
 score = 0
 
-scoreLabel = turtle.Turtle()
-scoreLabel.penup()
-scoreLabel.hideturtle()
-scoreLabel.goto(-280, 260)
-scoreLabel.write("Score: " + str(score), font=("Arial", 24, "normal"))
+scoreLable = turtle.Turtle()
+scoreLable.penup()
+scoreLable.hideturtle()
+scoreLable.goto(-280, 260)
+scoreLable.write("Score: " + str(score), font = ("Arial", 24, "normal"))
 
 def update_score():
-    scoreLabel.clear()
-    scoreLabel.write("Score: " + str(score), font=("Arial", 24, "normal"))
+    scoreLable.clear()
+    scoreLable.write("Score: " + str(score), font = ("Arial", 24, "normal"))
+
 
 snakeFood = turtle.Turtle()
 snakeFood.color("red")
@@ -33,25 +34,28 @@ snakeFood.shape("circle")
 snakeFood.shapesize(0.5, 0.5)
 snakeFood.speed(0)
 snakeFood.penup()
-snakeFood.goto(0, 100)
+snakeFood.goto(0,100)
+
 
 def move():
-    position = snakeHead.pos()
+    position = snakeHead.position()
+    
+
     if snakeHead.direction == "up":
         snakeHead.sety(snakeHead.ycor() + 20)
-    if snakeHead.direction == "down":
+    elif snakeHead.direction == "down":
         snakeHead.sety(snakeHead.ycor() - 20)
-    if snakeHead.direction == "right":
+    elif snakeHead.direction == "right":
         snakeHead.setx(snakeHead.xcor() + 20)
-    if snakeHead.direction == "left":
+    elif snakeHead.direction == "left":
         snakeHead.setx(snakeHead.xcor() - 20)
-    
+
     for segment in segments:
         newPosition = segment.position()
         segment.goto(position)
         position = newPosition
-    
-    if snakeHead.xcor() >= 290 or snakeHead.xcor() >= -290 or snakeHead.ycor() >= 290 or snakeHead.ycor() >= -290:
+
+    if snakeHead.xcor() >= 290 or snakeHead.xcor() <= -290 or snakeHead.ycor() >= 290 or snakeHead.ycor() <= -290:
         kill_snake()
     for segment in segments:
         if segment.distance(snakeHead) < 10:
@@ -62,45 +66,44 @@ def move():
 def move_up():
     if snakeHead.direction != "down":
         snakeHead.direction = "up"
-        
 def move_down():
     if snakeHead.direction != "up":
         snakeHead.direction = "down"
-
 def move_left():
     if snakeHead.direction != "right":
         snakeHead.direction = "left"
-
 def move_right():
     if snakeHead.direction != "left":
         snakeHead.direction = "right"
-        
+
 def kill_snake():
     global segments, delay, score
     for segment in segments:
         segment.color("red")
         time.sleep(delay)
-        snake_screen.update()
+        snakeScreen.update()
     time.sleep(1)
     for segment in reversed(segments):
         segment.hideturtle()
         time.sleep(delay)
-        snake_screen.update()
+        snakeScreen.update()
     segments = []
-    snakeHead.goto(0, 0)
+    snakeHead.goto(0,0)
     snakeHead.direction = "stop"
     delay = 0.1
     score = 0
     update_score()
+    
 
-snake_screen.onkeypress(move_up, 'w')
-snake_screen.onkeypress(move_down, 's')
-snake_screen.onkeypress(move_left, 'a')
-snake_screen.onkeypress(move_right, 'd')
-snake_screen.listen()
+
+snakeScreen.onkeypress(move_up, 'w')
+snakeScreen.onkeypress(move_down, 's')
+snakeScreen.onkeypress(move_left, 'a')
+snakeScreen.onkeypress(move_right, 'd')
+snakeScreen.listen()
 
 while True:
-    snake_screen.update()
+    snakeScreen.update()
     move()
     time.sleep(delay)
     if snakeHead.distance(snakeFood) < 15:
@@ -108,8 +111,8 @@ while True:
         y = random.randint(-270, 270)
         snakeFood.goto(x, y)
         snakeSegment = snakeHead.clone()
-        snakeSegment.color("grey")
+        snakeSegment.color("black")
         segments.append(snakeSegment)
         score = score + 10
         update_score()
-        
+
