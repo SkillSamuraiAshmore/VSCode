fff3b9b26ecaa0fc8c22f00a956365d00bf7da48
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-07-10 16:38:36 +1000

    started clear_rows function

--- MadelineP/tetris/starter_code.py
+++ MadelineP/tetris/starter_code.py
@@ -232,23 +232,35 @@ def draw_grid(surface, grid):
     #pass
 
 def clear_rows(grid, locked):
-    pass
+    inc = 0
+    for i in range(len(grid)-1, -1, -1):
+        row = grid[i]
+        if (0,0,0) not in row:
+            inc += 1
+            ind = i
+        if j in range(len(row)):
+            try:
+                del locked[(j,i)]
+            except:
+                continue
+                
+        
 
 def draw_next_shape(shape, surface):
     font = pygame.font.SysFont('comicsans', 30)
-    label = font.render('Next Shape:', 1, (255,255,255))
-    
+    label = font.render('Next Shape', 1, (255,255,255))
+
     sx = top_left_x + play_width + 50
     sy = top_left_y + play_height/2 - 100
     format = shape.shape[shape.rotation % len(shape.shape)]
-    
+
     for i, line in enumerate(format):
         row = list(line)
         for j, column in enumerate(row):
             if column == '0':
-               pygame.draw.rect(surface, shape.color, (sx + j*block_size, sy + i*block_size, block_size, block_size), 0) 
-               
-    surface.blit(label, (sx + 10, sy - 30))
+                pygame.draw.rect(surface, shape.color, (sx + j*30, sy + i*30, 30, 30), 0)
+
+    surface.blit(label, (sx + 10, sy- 30))
 
     
 
@@ -265,9 +277,7 @@ def draw_window(surface, grid):
             pygame.draw.rect(surface, grid[i][j], (top_left_x + j* 30, top_left_y + i * 30, 30, 30), 0)
             
     pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
-    
-    
-    
+
     draw_grid(surface, grid)
     pygame.display.update()
 
@@ -284,7 +294,9 @@ def main(win):
     fall_speed = 0.27
     
     while run:
-    
+        
+        
+        
         grid = create_grid(locked_position)
         fall_time += clock.get_rawtime()
         clock.tick()
@@ -339,8 +351,10 @@ def main(win):
                 
                 
                 
-                      
+                     
         draw_window(win, grid)
+        draw_next_shape(next_piece, win) 
+        pygame.display.update()
         
         if check_lost(locked_position):
             run = False
