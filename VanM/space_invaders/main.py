005c7d7fbcc5aa62d9be5c7077239f0b36fd1bde
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-07-23 16:20:39 +1000

    not working commit

--- VanM/space_invaders/main.py
+++ VanM/space_invaders/main.py
@@ -3,6 +3,22 @@ import random
 
 turtle.bgcolor("black")
 
+invaderList = []
+number_of_invaders = 5
+invaderspeed = 5
+
+for i in range(number_of_invaders):
+    invader = turtle.Turtle()
+    invader.color("purple")
+    invader.shape("square")
+    invader.speed(0)
+    invader.penup()   
+    x = random.randint(-200, 200)
+    y = random.randint(100, 200)
+    invader.setposition(x, y)
+    invaderList.append(invader)
+    
+
 player = turtle.Turtle()
 player.color("red")
 player.shape("arrow")
@@ -13,19 +29,56 @@ player.setposition(0, -250)
 
 playerspeed = 15
 
+bullet = turtle.Turtle()
+bullet.color("yellow")
+bullet.shape("arrow")
+bullet.speed(0)
+bullet.penup()
+bullet.setheading(90)
+bullet.shapesize(0.5, 0.5)
+bullet.hideturtle()
+
+bulletspeed = 25
+bulletstate = "ready"
+
+
 def move_left():
     x = player.xcor()
-    x = (x - playerspeed, -280)
+    x = max(x - playerspeed, -280)
     player.setx(x)
     
 def move_right():
     x = player.xcor()
-    x = min(x - playerspeed, 280)
+    x = min(x + playerspeed, 280)
     player.setx(x)
+
+def fire_bullet():
+    global bulletstate
+    if bulletstate == "ready":
+        # bullet is ready to fire 
+        bulletstate = "fire"
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setposition(x, y)
+        bullet.showturtle()
+        
 
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+turtle.onkey(fire_bullet, "space")
+
+while True:
+    if bulletstate == "fire":
+        # bullet has been fired
+        y = bullet.ycor
+        y = y + bulletspeed
+        bullet.sety(y)
+        
+    if bullet.ycor() > 275:
+        bullet.hideturtle() 
+        bulletstate = "ready"
+        
 
 
 
