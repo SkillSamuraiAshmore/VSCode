e2bce412565162da7f3795e4651b29bd3c0150a7
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-08-02 16:19:56 +1000

    finished check_position

--- MadelineP/Pac-man/main.py
+++ MadelineP/Pac-man/main.py
@@ -60,6 +60,8 @@ player_x = 450
 player_y = 663
 direction = 0
 counter = 0
+flicker = False
+turns_allowed = [False, False, False, False]
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -69,7 +71,7 @@ def draw_board():
             if level[i][j] == 1:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
             
-            if level[i][j] == 2:
+            if level[i][j] == 2 and not flicker:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5 * num1)), 10)    
                 
             if level[i][j] == 3:
@@ -103,29 +105,95 @@ def draw_player():
         screen.blit(player_images[counter // 5], (player_x, player_y))  
         
     elif direction == 1:
-        screen.blit(pygame.transorm.flip(player_images[counter // 5], True, False), (player_x, player_y))  
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))  
     
     elif direction == 2:
-        screen.blit(pygame.transorm.rotate(player_images[counter // 5], 90), (player_x, player_y))  
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))  
     
     elif direction == 3:
-        screen.blit(pygame.transorm.rotate(player_images[counter // 5], 270), (player_x, player_y))  
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))  
         
+def check_position(centerx, centery):
+    turns = [False, False, False, False ]
+    num1 = (HEIGHT - 50)//32
+    num2 = (WIDTH//30)
+    num3 = 15
+    
+    if centerx //30 < 29:
+        if direction == 0:
+            if level[centery//num1][(centerx - num3)// num2] < 3:
+                turns[1] = True
+        if direction == 1:
+            if level[centery//num1][(centerx + num3)// num2] < 3:
+                turns[0] = True
+        if direction == 2:
+            if level[(centery + num3)//num1][centerx// num2] < 3:
+                turns[3] = True
+        if direction == 3:
+            if level[(centery - num3)//num1][centerx // num2] < 3:
+                turns[2] = True
         
-             
-            
-                
+        if direction == 2 or direction == 3:
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num3)//num1][(centerx - num2) // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num3)//num1][centerx // num2] < 3:
+                    turns[2] = True
+                    
+            if 12 <= centerx % num1 <= 18:
+                if level[centery//num1][centerx // num2] < 3:
+                    turns[1] = True
+                if level[centery//num1][(centerx + num2) // num2] < 3:
+                    turns[0] = True
+                    
+        if direction == 0 or direction == 1:
+            if 12 <= centerx % num2 <= 18:
+                if level[(centery + num1)//num1][(centerx - num2) // num2] < 3:
+                    turns[3] = True
+                if level[(centery - num1)//num1][centerx // num2] < 3:
+                    turns[2] = True
+                    
+            if 12 <= centerx % num1 <= 18:
+                if level[centery//num1][(centerx  - num3) // num2] < 3:
+                    turns[1] = True
+                if level[centery//num1][(centerx + num3) // num2] < 3:
+                    turns[0] = True
+              
+    else:
+        turns[0] = True
+        turns[1] = True
+    return turns
 
 run = True
 while run:
     timer.tick(fps)
+    if counter < 19:
+        counter += 1
+        if counter > 15:
+            flicker = False
+    else:
+        counter = 0
+        flicker = True
     screen.fill('black')
     draw_board()
     draw_player()
+    center_x = player_x + 23
+    center_y = player_y + 24
+    turns_allowed = check_position(center_x, center_y)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                direction = 0
+            if event.key == pygame.K_LEFT:
+                direction = 1
+            if event.key == pygame.K_UP:
+                direction = 2
+            if event.key == pygame.K_DOWN:
+                direction = 3
             
     pygame.display.flip()
     
