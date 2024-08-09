ba4890b29b157b2b0254a1674f1c49cb30be6873
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-08-09 15:45:02 +1000

    finished player movement

--- MadelineP/Pac-man/main.py
+++ MadelineP/Pac-man/main.py
@@ -163,57 +163,6 @@ def check_position(centerx, centery):
 
     return turns
 
-# def check_position(centerx, centery):
-#     turns = [False, False, False, False ]
-#     num1 = (HEIGHT - 50)//32
-#     num2 = (WIDTH//30)
-#     num3 = 15
-    
-#     if centerx //30 < 29:
-#         if direction == 0:
-#             if level[centery//num1][(centerx - num3)// num2] < 3:
-#                 turns[1] = True
-#         if direction == 1:
-#             if level[centery//num1][(centerx + num3)// num2] < 3:
-#                 turns[0] = True
-#         if direction == 2:
-#             if level[(centery + num3)//num1][centerx// num2] < 3:
-#                 turns[3] = True
-#         if direction == 3:
-#             if level[(centery - num3)//num1][centerx // num2] < 3:
-#                 turns[2] = True
-        
-#         if direction == 2 or direction == 3:
-#             if 12 <= centerx % num2 <= 18:
-#                 if level[(centery + num3)//num1][(centerx - num2) // num2] < 3:
-#                     turns[3] = True
-#                 if level[(centery - num3)//num1][centerx // num2] < 3:
-#                     turns[2] = True
-                    
-#             if 12 <= centerx % num1 <= 18:
-#                 if level[centery//num1][centerx // num2] < 3:
-#                     turns[1] = True
-#                 if level[centery//num1][(centerx + num2) // num2] < 3:
-#                     turns[0] = True
-                    
-#         if direction == 0 or direction == 1:
-#             if 12 <= centerx % num2 <= 18:
-#                 if level[(centery + num1)//num1][(centerx - num2) // num2] < 3:
-#                     turns[3] = True
-#                 if level[(centery - num1)//num1][centerx // num2] < 3:
-#                     turns[2] = True
-                    
-#             if 12 <= centerx % num1 <= 18:
-#                 if level[centery//num1][(centerx  - num3) // num2] < 3:
-#                     turns[1] = True
-#                 if level[centery//num1][(centerx + num3) // num2] < 3:
-#                     turns[0] = True
-              
-#     else:
-#         turns[0] = True
-#         turns[1] = True
-#     return turns
-
 def move_player(play_x, play_y):
     if direction == 0 and turns_allowed[0]:
         play_x += player_speed
@@ -269,14 +218,14 @@ while run:
                 direction_command = direction
         
         
-        for i in range(4):
+    for i in range(4):
             if direction_command == i and turns_allowed[i]:
                 direction = i 
                     
-        if player_x > 900:
-            player_x = -47
-        elif player_x < -50:
-            player_x = 897
+    if player_x > 900:
+        player_x = -47
+    elif player_x < -50:
+        player_x = 897
         
             
     pygame.display.flip()
