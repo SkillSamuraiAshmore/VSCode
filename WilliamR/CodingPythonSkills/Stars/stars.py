aa2632c30a10e8b2396e08ff7afb1b3b49ffa5b1
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-08-15 15:06:04 +1000

    William

--- WilliamR/CodingPythonSkills/Stars/stars.py
+++ WilliamR/CodingPythonSkills/Stars/stars.py
@@ -6,12 +6,100 @@ star_count = input("how many stars do you want to see ")
 star_count = int(star_count )
 
 speed("fastest")
+
 setup(800, 800)
 bgcolor("dark blue")
 penup()
 
 for star_num in range(star_count):
-    pass
+    x_pos = randint(-400, 400)
+    y_pos = randint(-400, 400)
+
+    size = randint(5, 20)
+    colour = choice(['white', 'papaya whip'])
+
+    goto(x_pos, y_pos)
+
+    star(size, colour)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
