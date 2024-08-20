80d9722382b4cc9451e82756ad9862a55cd9ef8c
SkillSamuraiAshmore <skillsamuraiashmore@gmail.com>
2024-08-20 16:34:39 +1000

    Vann Progress

--- VanM/snake/main.py
+++ VanM/snake/main.py
@@ -14,7 +14,20 @@ snakeHead.speed(0)
 snakeHead.direction = "stop"
 
 delay = 0.2
-snakeSegments[]
+snakeSegments = []
+score = 0
+
+scoreLabel=turtle.Turtle()
+scoreLabel.penup()
+scoreLabel.hideturtle()
+scoreLabel.goto(-280, 260)
+scoreLabel.write("Score: " + str(score), font = ("Arial", 24, "normal"))
+
+def update_score():
+    scoreLabel.clear()
+    scoreLabel.write("Score: " + str(score), font = ("Arial", 24, "normal"))
+
+
 
 food = turtle.Turtle()
 food.color("yellow")
@@ -25,6 +38,9 @@ food.penup()
 food.goto(0,100)
 
 def move():
+    
+    position = snakeHead.position()
+    
     if snakeHead.direction == "up":
         snakeHead.sety(snakeHead.ycor() + 20)
     if snakeHead.direction == "down":
@@ -33,6 +49,22 @@ def move():
         snakeHead.setx(snakeHead.xcor() + 20)
     if snakeHead.direction == "left":
         snakeHead.setx(snakeHead.xcor() + - 20)
+        
+        
+    for segment in snakeSegments:
+        newPosition = segment.position()
+        segment.goto(position)
+        position = newPosition
+        
+    if snakeHead.xcor() >= 290 or snakeHead.xcor() <= -290 or snakeHead.ycor() >= 290 or snakeHead.ycor() <= -290:
+        operation_Kill_snake()
+    for segment in snakeSegments:
+        if segment.distance(snakeHead) < 10:
+            operation_Kill_snake()
+            
+            
+            
+                          
 def move_up():
     if snakeHead.direction != "down":
         snakeHead.direction = "up"
@@ -44,7 +76,32 @@ def move_left():
         snakeHead.direction = "left"
 def move_right():
      if snakeHead.direction != "left":
-        snakeHead.direction = "right"       
+        snakeHead.direction = "right"
+        
+        
+def operation_Kill_snake():
+            global segments, delay, score
+            for segment in segments:
+                segment.color("yellow")
+                time.sleep(delay)
+                snakeScreen.update()
+            time.sleep(1)
+            for segment in reversed(segments):
+                segment.hideturtle
+                time.sleep(delay)
+                snakeScreen.update()
+                
+            segments = []
+            snakeHead.goto(0,0)
+            snakeHead.direction
+            delay = 0.1
+            score = 0 
+            update_score()
+                
+        
+        
+        
+               
 snakeScreen.onkeypress(move_up , 'w')
 snakeScreen.onkeypress(move_down , 's')
 snakeScreen.onkeypress(move_left , 'a')
@@ -61,4 +118,6 @@ while True:
         food.goto(x , y)
         snakeSegment = snakeHead.clone()
         snakeSegment.color("green")
-        segments.append(snakeSegment)
+        snakeSegments.append(snakeSegment)
+        score = score + 10
+        update_score()
