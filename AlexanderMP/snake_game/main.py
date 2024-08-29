2bd966341485904542c93ba0990ffd667339b73a
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-08-29 15:45:53 +1000

    Alexander Snake - Double Code Removed and Debugged

--- AlexanderMP/snake_game/main.py
+++ AlexanderMP/snake_game/main.py
@@ -27,65 +27,39 @@ snakeFood.goto(0,100)
 
 def move():
     position = snakeHead.position()
-    
-    
 
     if snakeHead.direction == "up":
-        snakeHead.sety(snakeHead.ycor()+ 20)
-    if snakeHead.direction == "down":
-        snakeHead.sety(snakeHead.ycor()- 20)
-    if snakeHead.direcion == "right":
-        snakeHead.setx(snakeHead.xcor()+ 20)    
-    if snakeHead.direction == "left":
-        snakeHead.setx(snakeHead.xcor()- 20)    
+        snakeHead.sety(snakeHead.ycor() + 20)
+    elif snakeHead.direction == "down":
+        snakeHead.sety(snakeHead.ycor() - 20)
+    elif snakeHead.direction == "right":
+        snakeHead.setx(snakeHead.xcor() + 20)
+    elif snakeHead.direction == "left":
+        snakeHead.setx(snakeHead.xcor() - 20)
+
+    for segment in segments:
+        newPosition = segment.position()
+        segment.goto(position)
+        position = newPosition
         
 def move_up():
     if snakeHead.direction != "down":
         snakeHead.direction = "up"
-        
 def move_down():
     if snakeHead.direction != "up":
         snakeHead.direction = "down"
-        
 def move_left():
     if snakeHead.direction != "right":
         snakeHead.direction = "left"
-        
 def move_right():
     if snakeHead.direction != "left":
         snakeHead.direction = "right"
         
-snakeScreen.onkeypress(move_up   , 'w'   )        
+snakeScreen.onkeypress(move_up   , 'w' )        
 snakeScreen.onkeypress(move_down , 's' )        
 snakeScreen.onkeypress(move_left , 'a' )    
-snakeScreen.onkeypress(move_right, 'd'   )  
-  
-def move():
-    if snakeHead.direction == "up":
-        snakeHead.sety(snakeHead.ycor()+ 20)
-    if snakeHead.direction =="down":
-        snakeHead.sety(snakeHead.ycor()- 20)
-    if snakeHead.direction =="right":
-        snakeHead.setx(snakeHead.xcor()+ 20)
-    if snakeHead.direction =="left":
-        snakeHead.setx(snakeHead.xcor()- 20)       
-def move_up():
-    if snakeHead.direction != "down":
-        snakeHead.direction = "up" 
-def move_down():
-    if snakeHead.direction != "up":
-        snakeHead.direction = "down"
-def move_left():
-    if snakeHead.direction != "right":
-        snakeHead.direction = "left"
-def move_right():
-    if snakeHead.direction != "left":
-        snakeHead.direction = "right"    
-        
-snakeScreen.onkeypress(move_up , 'w')    
-snakeScreen.onkeypress(move_down , 's')   
-snakeScreen.onkeypress(move_right , 'd')    
-snakeScreen.onkeypress(move_left , 'a')
+snakeScreen.onkeypress(move_right, 'd' )  
+   
 snakeScreen.listen()
 
 
