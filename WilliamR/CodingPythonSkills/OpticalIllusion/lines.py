fdbd63c7a20d1eb02451d9d8833c722935dab19b
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2024-09-30 16:07:04 +1000

    .

--- WilliamR/CodingPythonSkills/OpticalIllusion/lines.py
+++ WilliamR/CodingPythonSkills/OpticalIllusion/lines.py
@@ -2,26 +2,29 @@ import turtle
 from  helperfile import drawLine, drawCircle
 turtle.speed(0)
 turtle.pensize(2)
-#inside circle
+
 drawLine(turtle, -250, 100, -50, 100, "purple")
-drawCircle(turtle,-250, 100, 20, "purple", angle=270, fill=False)
-drawCircle(turtle,-50, 100, 20, "purple", angle=90, fill=False)
-#out side circle
+drawCircle(turtle, -250, 100, 20, "purple", angle=270, fill=False)
+drawCircle(turtle, -50, 100, 20, "purple", angle=90, fill=False)
+
 drawLine(turtle, 50, 100, 250, 100, "purple")
-drawCircle(turtle, 50, 100, 20, "purple",angle=90, fill=False)
-drawCircle(turtle, 250, 100, 20, "purple",angle=270, fill=False)
+drawCircle(turtle, 50, 100, 20, "purple", angle=90, fill=False)
+drawCircle(turtle, 250, 100, 20, "purple", angle=270, fill=False)
 
-# outward ticks
-drawLine(turtle, -250, -100, 250, -100, "purple")
+drawLine(turtle, -250, -100, -50, -100, "purple")
+drawLine(turtle, -250, -100, -280, -70, "purple")
+drawLine(turtle, -250, -100, -280, -130, "purple")
+drawLine(turtle, -50, -100, -20, -130, "purple")
+drawLine(turtle, -50, -100, -20, -70, "purple")
 
 
-#inward ticks
-drawLine(turtle, 50, -100, 250, 100, "purple")
-drawLine(turtle, 50, -100, -80, 70, "purple")
-drawLine(turtle, 50, -100, -80, 130, "purple")
-drawLine(turtle, 250, -100, 220, 70, "purple")
-drawLine(turtle, 50, 100, 250, 100, "purple")
 
+
+drawLine(turtle, 50, -100, 250, -100, "purple")
+drawLine(turtle, 50, -100, 80, -70, "purple")
+drawLine(turtle, 50, -100, 80, -130, "purple")
+drawLine(turtle, 250, -100, 220, -70, "purple")
+drawLine(turtle, 250, -100, 220, -130, "purple")
 
 
 
