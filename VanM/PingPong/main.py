5918f4c9a8d3ff037ea456c7d73656d3cfa760c7
SkillSamuraiAshmore <skillsamuraiashmore@gmail.com>
2024-10-15 16:20:39 +1000

    paddle b works

--- VanM/PingPong/main.py
+++ VanM/PingPong/main.py
@@ -90,3 +90,8 @@ while True:
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= 1
+        
+    # paddle and ball collisons
+    if ball.xcor() > 340 and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+        ball.dx *= -1 
+    if ball.xcor() > 340 and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
