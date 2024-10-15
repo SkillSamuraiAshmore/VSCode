bb42afd84997735892c16d38da41cb1a6148fee0
SkillSamuraiAshmore <skillsamuraiashmore@gmail.com>
2024-10-15 15:36:54 +1000

    ball and paddles designed

--- VanM/PingPong/main.py
+++ VanM/PingPong/main.py
@@ -12,11 +12,27 @@ win.tracer(0)
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
-# TODO: paddle b
+# paddle b
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 #ball 
-
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 #Main_Game_Loop
 while True:
