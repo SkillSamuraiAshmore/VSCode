ee1c2921eae7bc4f9bc2ca295ab7530eae5ff51f
SkillSamuraiAshmore <skillsamuraiashmore@gmail.com>
2024-11-05 16:27:45 +1000

    17:10

--- VanM/Wordle/main.py
+++ VanM/Wordle/main.py
@@ -7,15 +7,20 @@ def main():
     print("wordle")
     
     
-    while True:
+    while wordle.can_attempt:
         x = input("Type your guess: ")
+        wordle.attempt(x)
         if x == wordle.secret:
             print("You have guessed the word!")
             break
+        print("Womp Womp you're wrong😂")
         
         
-        
-
+        if wordle.is_solved:
+            print("Yay you solved the puzzle😱!")
+        else:
+            print("Failure you lost💀")
+            
 if __name__ == "__main__":
     main()
 
