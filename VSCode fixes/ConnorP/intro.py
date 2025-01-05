5bac3cebe25b439608b531147963ef91d354988c
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-01-06 08:59:02 +1000

    yep

---
+++ VSCode fixes/ConnorP/intro.py
@@ -0,0 +1,7 @@
+Correct_answers = 0        
+Fav_Game = input("Can hippos swim")
+if Fav_Game.lower() == "no": 
+    print ("Correct") Correct_answers += 1
+else:
+    print("Wrong")
+print ("Correct_answers")  
