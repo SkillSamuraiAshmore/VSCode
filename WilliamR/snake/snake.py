26dd42a1c597ba2859e9243974034f3b467210c3
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-02-24 16:29:13 +1000

    troubleshoot problems with wasd

--- WilliamR/snake/snake.py
+++ WilliamR/snake/snake.py
@@ -13,9 +13,50 @@ snakeHead.penup()
 snakeHead.speed(0)
 snakeHead.direction ="stop"
 
+delay = 0.2
+
+snakeFood = turtle.turtle
+snakeFood.color("red")
+snakeFood.shape
+snakeFood.
+snakeFood.
+snakeFood.
 def move(): 
     if snakeHead.direction == "up":
         snakeHead.sety(snakeHead.ycor() + 20)
     if snakeHead.direction == "down":
-        snakeHead.sety(snakeHead.ycor)
-turtle.done()
+        snakeHead.sety(snakeHead.ycor() - 20)
+    if snakeHead.direction == "right":  
+        snakeHead.setx(snakeHead.xcor() + 20)
+    if snakeHead.direction == "left":      
+        snakeHead.setx(snakeHead.xcor() - 20)
+        
+def move_up():       
+     if snakeHead.direction!="down":
+         snakeHead.direction = "up"   
+def move_down():
+    if snakeHead.direction!="up":
+        snakeHead.direction = "down"        
+def move_left():
+    if snakeHead.direction!="right":
+        snakeHead.direction = "left"        
+def move_right():
+    if snakeHead.direction!="left":
+        snakeHead.direction = "right" 
+               
+snakeScreen.onkeypress(move_up, 'w')
+snakeScreen.onkeypress(move_down , 's')
+snakeScreen.onkeypress(move_left , 'a')
+snakeScreen.onkeypress(move_right , 'd')
+snakeScreen.listen()
+
+
+
+
+
+while True:
+    snakeScreen.update()
+    move()
+    time.sleep(delay)
+       
+#turtle.done()     
