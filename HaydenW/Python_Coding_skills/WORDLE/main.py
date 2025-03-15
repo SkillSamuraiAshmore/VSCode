a7d110b5fb429e5bbef5aca48e5e6fd1bf480860
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-03-15 11:40:43 +1000

    5t

--- HaydenW/Python_Coding_skills/WORDLE/main.py
+++ HaydenW/Python_Coding_skills/WORDLE/main.py
@@ -1,16 +1,20 @@
+from mainX2 import  Wordle
+
 def main():
     print("hello wordle")
-    wordle = Wordle("Apple")
+    wordle = Wordle("spain")
     
     
     
-    while True:
+    while wordle.can_attempt:
         x = input("Type your guess: ")
-        if x == wordle.secret:
-            print("you have guessed the rigt word!!!")
-            break
-        print("Your guess is incorrect")
+        wordle.attempt(x)
+      
+        if wordle.is_solved:
+            print("You've solved the puzzle!")
+        else:
+            print("YOU FAILED TO SOLVE THE PUZZLE... YOU STUPID!!!")
     
     
-if __name__ == "__main_":
+if __name__ == "__main__":
     main()
