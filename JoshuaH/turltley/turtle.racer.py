c5149e7ff156d139a097f72ab5d3bbb7ea1976b3
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-03-19 16:37:01 +1000

    finished turtle racer

--- JoshuaH/turltley/turtle.racer.py
+++ JoshuaH/turltley/turtle.racer.py
@@ -49,6 +49,33 @@ t1.shape("turtle")
 t1.penup()
 t1.goto(-250, 100)
 
+t2 = Turtle()
+t2.speed(0)
+t2.color("blue")
+t2.shape("turtle")
+t2.penup()
+t2.goto(-250, 50)
+
+t3 = Turtle()
+t3.speed(0)
+t3.color("red")
+t3.shape("turtle")
+t3.penup()
+t3.goto(-250, 0)
+
+t4 = Turtle()
+t4.speed(0)
+t4.color("yellow")
+t4.shape("turtle")
+t4.penup()
+t4.goto(-250, -50)
+
+time.sleep(2)
+
+for i in range (140):
+    t1.forward(randint(1,6))
+    t2.forward(randint(1,6))
+    t3.forward(randint(1,6))
+    t4.forward(randint(1,6))
+    
 turtle.done()
-    
-    
