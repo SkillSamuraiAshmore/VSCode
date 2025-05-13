0470efcc8b51716c306c68767fd6b83fddd9a12a
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-05-13 17:31:30 +1000

    hello

--- MiraH/Turtle/main.py
+++ MiraH/Turtle/main.py
@@ -1,7 +1,20 @@
-age = 20
+age = 12
 temperature = 25.39
 country = "Australia"
 is_day = True
 print(temperature)
 
-print(country)
+country_name = "Australia"
+population = 26000000
+landblocked = False
+border_size = 60000.256
+health = 100
+
+health = 10
+print (health)
+health = 30
+health = 30.0
+print(health)
+
+if age == 12:
+  a = 10
