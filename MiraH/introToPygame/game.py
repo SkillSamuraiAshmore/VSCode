809ee6fa1efff044ea309b72d5c9c4fbe97262e1
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-06-10 17:19:35 +1000

    clock and screen

--- MiraH/introToPygame/game.py
+++ MiraH/introToPygame/game.py
@@ -3,6 +3,12 @@ import pygame, sys
 
 pygame.init()
 
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode
+pygame.display.set_caption("My Game")
+
+
 while True:
   for event in pygame.event.get():  
     if event.type == pygame.QUIT:
@@ -10,4 +16,11 @@ while True:
       
       sys.exit()
   
-      
+  screen.fill("white")    
+  pygame.display.flip()
+  
+    clock.tick(60)
+    previous_time = clock.get_time()
+    fps = clock.get_fps()
+    print(previous_time)
+    print = (fps)
