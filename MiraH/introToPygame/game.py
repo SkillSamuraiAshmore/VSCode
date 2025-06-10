f4be8e0af43ba6d2618307becc1e7b3fb487db02
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-06-10 17:26:23 +1000

    colo

--- MiraH/introToPygame/game.py
+++ MiraH/introToPygame/game.py
@@ -5,9 +5,10 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode
+screen = pygame.display.set_mode()
 pygame.display.set_caption("My Game")
 
+white = pygame.Color(255, 255, 255, 1)
 
 while True:
   for event in pygame.event.get():  
@@ -16,10 +17,11 @@ while True:
       
       sys.exit()
   
-  screen.fill("white")    
+  screen.fill(white)    
   pygame.display.flip()
   
     clock.tick(60)
+    
     previous_time = clock.get_time()
     fps = clock.get_fps()
     print(previous_time)
