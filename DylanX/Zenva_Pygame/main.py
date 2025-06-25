6404e0130a7fce25d71e75e2d874783f558d6040
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-06-25 17:02:03 +1000

    pygame colours cool so  cool

--- DylanX/Zenva_Pygame/main.py
+++ DylanX/Zenva_Pygame/main.py
@@ -9,6 +9,7 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode()
 pygame.display.set_caption("My special game ")
 red=pygame.Color(255,0,0,1)
+grey=pygame.Color(100,100,100,1)
 #GAME LOOP
 
 while True:
@@ -19,7 +20,7 @@ while True:
             #QUIT
             pygame.quit()
             sys.exit()
-    screen.fill(red)
+    screen.fill(cyan)
     pygame.display.flip()
     #pygame.display.update()
     #SET FPS
