80cdd3ab765fb0571f0a77cbc406de7c5a78cc5d
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-06-25 16:29:34 +1000

    done the code it really pmo

--- JoshuaH/new_Py/ai_project/main.py
+++ JoshuaH/new_Py/ai_project/main.py
@@ -1,16 +1,15 @@
 import openai
 from openai import OpenAI
+import os
 
 # pass the api key 
 # never upload this to github
 client = OpenAI(
     #TODO: i think it's saying error 429 because we are sharing this key online, later the tutorial hides this key so it should be fine later at the end of the tutorial
-    api_key = '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'
+    api_key = os.environ.get('OPENAI_API_KEY')
 )
-
 # define prompt 
 messages = []
-messages.append({'role': 'system', 'content' : 'you are a CTO mentoring deveoplers dont onloy provide answers also ask guiding questions and make sure to get the LOOOOOOOOOOOOW TAPER FADE'})
 messages.append({'role': 'user', 'content': 'why is my website down?'})
 
 try:
