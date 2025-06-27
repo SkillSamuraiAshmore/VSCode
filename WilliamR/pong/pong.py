8fb26a3556303a59fc3582dcc79950242283c91a
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-06-28 08:33:35 +1000

    t

--- WilliamR/pong/pong.py
+++ WilliamR/pong/pong.py
@@ -29,8 +29,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.5
-ball.dy = 0.5
+ball.dx = 0.1
+ball.dy = -0.4
 
 
 #function
@@ -66,38 +66,23 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 #border checking
-    if ball.ycor > 290:   
+    if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
    
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
    
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
+   #paddle ball colisions
+    if (ball.xcor() > 340 and ball.xcor < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *=1
