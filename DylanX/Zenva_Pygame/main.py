825b8f8cf82690bbb1b51c63389c3bd501658c89
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-06-28 11:56:41 +1000

    s

--- DylanX/Zenva_Pygame/main.py
+++ DylanX/Zenva_Pygame/main.py
@@ -1,49 +1,41 @@
-
-import pygame,sys,math
+import pygame, sys, math
 
 pygame.init()
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode()
-pygame.display.set_caption("My special game ") 
+screen = pygame.display.set_mode((800, 500))
+pygame.display.set_caption("My Special Game")
 
-grey=pygame.Color(100,100,100,1)
-white=pygame.Color(255,255,255,1)
+background_color = "white"
 
-#apple=pygame.image.load("assets/apple.png")
-#apple_rect=apple.get_rect()
-#apple_rect.x=50
-#apple_rect.y=100
+apple = pygame.image.load("apple.png")
+rect = apple.get_rect()
 
-#rect  = pygame.Rect(500,10,200,100)
-#triangle_coords= [(100,50),(125,100) ,(175,100),(75,100)]
+rect = rect.move(10, 10)
+
+screen_rect = screen.get_rect()
+
+
+def check_for_collision():
+    return rect.x <= 0 or rect.x + rect.width >= screen_rect.width or rect.y <= 0 or rect.y + rect.height >= screen_rect.height
+
 
 while True:
-    for event in  pygame.event.get():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screen.fill(white)
     
-    pygame.draw.arc(screen,grey,pygame.Rect(50,50,100,100),0,math.pi,5)
+    if not check_for_collision(): 
+        rect = rect.move(0, 1) 
     
-    #pygame.draw.ellipse(screen,grey,pygame.Rect(50,50,100,200))
-    #pygame.draw.rect(screen,grey,pygame.Rect(50,50,100,200),1)
-    
-    #pygame.draw.lines(screen,grey,False,[(50,250),(50,50),(250,250),(250,50)],5)
-    #pygame.draw.line(screen,grey,(50,50),(200,100),5)
-    
-    #pygame.draw.rect(screen,grey,apple_rect,1)
-    #screen.blit(apple, apple_rect)
-    
-    #pygame.draw.circle(screen,grey,(100,100),50)
-    
-    #pygame.draw.rect(screen,grey,rect, 5)
-    
-    #pygame.draw.polygon(screen,grey,triangle_coords)
+    screen.fill(background_color)
+
+    screen.blit(apple, rect)
     
     pygame.display.flip()
+    
     clock.tick(60)
     
 
