8682bc80167a2a4a4ec3ecb857e7ab81992564a9
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-07-18 15:56:44 +1000

    all stuff

--- DylanX/python_platformer/main.py
+++ DylanX/python_platformer/main.py
@@ -14,14 +14,15 @@ window = pygame.display.set_mode((WIDTH,HEIGHT))
 
 def main(window):
     clock = pygame.time.Clock()
+    run=True
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run=False
                 break
-            pygame.quit()
-            quit()
+    pygame.quit()
+    quit()
             
 
 if __name__ == "__main__":
