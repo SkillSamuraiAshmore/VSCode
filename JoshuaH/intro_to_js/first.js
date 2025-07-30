fe434bcf2f73d9847bc1d2943018e0715ad67042
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-07-30 17:56:08 +1000

    did more js

--- JoshuaH/intro_to_js/first.js
+++ JoshuaH/intro_to_js/first.js
@@ -1,7 +1,84 @@
 //addition
-let a = 1+1;
-console.log (a);
+// let a = 1+1;
+// console.log (a);
 
-let b = 10;
-let c = a + b 
-console.log(c)
+// let b = 10;
+// let c = a + b ;
+// console.log("c", c);
+
+// b = b+1;
+// //b +=1
+// console.log("b", b);
+
+// //subtraction
+// let x = 10 - 5;
+// console.log("x",x);
+
+// let y = b + x;
+// console.log("y",y);
+
+// x = x - 1;
+//x -= 1;
+// console.log(x);
+
+// //multiplication
+// let unitprice = 2;
+// let units = 10;
+// let total = unitprice * units;
+// console.log(total);
+
+// //divison 
+// let n = 10;
+// let  result = n / 2;
+
+// //modulus 
+// let r = 5 % 2;
+// console.log(r);
+
+//challenge
+// let baseweight = 10000;
+// let foodweight = 100;
+// let passengerweight = 10;
+
+// let fuelunitweight = 2;
+// let fuelunits = 100;
+
+// let fuelweight = fuelunitweight * fuelunits; 
+
+// let totalweight = baseweight + foodweight + passengerweight + fuelweight;
+
+// console.log(totalweight);
+
+// let direction = 'right';
+
+// if (direction == 'left') {
+//     console.log('red planet') 
+// }
+
+// else if (direction == 'right') {
+//     console .log('purple planet')
+// }
+
+// else if (direction == 'backwards') {
+//     console .log('pick a direction there no turning back now')
+// }
+
+// else {
+//     console.log('ship crashes')
+// }
+
+let item = 'engine';
+
+if(item != 'engine') {
+    console.log('not engine!');
+}
+
+let score = 50;
+
+if (score > 60) {
+    console.log('pass');
+}
+
+else {
+    console.log('not pass')
+}
