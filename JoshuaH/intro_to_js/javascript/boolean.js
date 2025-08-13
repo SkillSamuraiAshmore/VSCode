5a5485de53961ddc963f5f78b3c4782780d76160
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-08-13 17:45:44 +1000

    did bool conditions

---
+++ JoshuaH/intro_to_js/javascript/boolean.js
@@ -0,0 +1,12 @@
+let distance = 250;
+let fuel = 100;
+distanceCondition = distance <= 200 && distance >= 100;
+
+if(distance = 200) {
+    console.log('wont make it');
+}
+
+else if(distanceCondition && fuel >=100) {
+console.log('you will make it')
+}
+
