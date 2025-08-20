d420432ec353c9add9e05d0c4e5f34a17ef6e1b3
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-08-20 17:48:09 +1000

    finished booleans

--- JoshuaH/intro_to_js/javascript/boolean.js
+++ JoshuaH/intro_to_js/javascript/boolean.js
@@ -1,12 +1,39 @@
-let distance = 250;
-let fuel = 100;
-distanceCondition = distance <= 200 && distance >= 100;
+// let distance = 250;
+// let fuel = 100;
+// let distanceCondition = distance <= 200 && distance >= 100;
+// let isEngineBroken = true
+// let isEngineFunctioning = false;
 
-if(distance = 200) {
-    console.log('wont make it');
+// if (!isEngineFunctioning || distance > 200) {
+//     console.log('wont make it')
+// }
+
+// else if(distance = 200) {
+//     console.log('wont make it');
+// }
+
+// else if(distanceCondition && fuel >=100) {
+// console.log('you will make it')
+// }
+
+// else if(distance < 100 && fuel >= 25) {
+//     console.log('you will make it')
+// }
+
+function hourToMinutes(hours) {
+    let result = hours * 60;
+    console.log(hours);
+    return result;
 }
 
-else if(distanceCondition && fuel >=100) {
-console.log('you will make it')
+let a = hourToMinutes(10);
+let b = hourToMinutes(20);
+
+let dayToHours = function(days) {
+
 }
 
+
+
+
+// console.log(a);
