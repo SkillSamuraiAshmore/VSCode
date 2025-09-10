d5ee5ff0f5843ca01578a8da8ce9b0b3eca65fd3
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-09-10 16:29:14 +1000

    38:27

--- WilliamR/wordle/play_wordle.py
+++ WilliamR/wordle/play_wordle.py
@@ -1,13 +1,15 @@
 from wordle import Wordle
 from colorama import Fore
+from letter_state import LetterState
 
 def main():
     print("hello wordle!")
+    #instantiating the Wordle class (building the house)
     wordle = Wordle("APPLE")
    
     
     while wordle.can_attempt:
-        x = input("type your guess: ")   
+        x = input("\ntype your guess: ")   
         if len(x) != wordle.Word_length:
             print(Fore.RED + f"word MUST be {wordle.Word_length} letters  long !!!!!!!" + Fore.RESET)
             continue
@@ -19,10 +21,15 @@ def main():
         print("you failled to solve the puzzle")
     
     
-def display_results(Wordle):
-    for word in Wordle.attempts:
-        result = Wordle.guess(word)
-    pass
+def display_results(wordle: Wordle):
+    print("\nresults.....\n")
+    print(f"You Have {wordle.remaining_attempts } attempts remaining\n")
+    for word in wordle.attempts:
+        result = wordle.guess(word)
+        colored_result_str = convert_result_to_color
+        print(colored_result_str)
+    for _ in range(wordle.remaining_attempts):
+        print(" ".join (["_"] * wordle.WORD_LENGTH))
 
 def convert_result_to_color(result: list[LetterState]):
     result_with_color = []
@@ -32,9 +39,11 @@ def convert_result_to_color(result: list[LetterState]):
         elif letter.is_in_word:
             color = Fore.YELLOW
         else:
-            coulor = Fore.LIGHTWHITE_EX
-            
-            
+            coulor = Fore.WHITE
+        colored_letter = color + letter.character + Fore.RESET
+        result_with_color.append(colored_letter)
+    return "".join(result_with_color)
+def draw_border_around         
             
             
 if __name__ == "__main__":
--- WilliamR/wordle/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-class Wordle:
-    
-    Max_Attempts = 6
-    Word_length = 5
