0408690814905d2f5713db2e0bad0d9656a917d8
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-10-08 17:22:04 +1000

    did methods and objects

--- JoshuaH/intro_to_js/javascript/ja.js
+++ JoshuaH/intro_to_js/javascript/ja.js
@@ -1,15 +1,44 @@
-let player = {age: 99, name:'ABC', isActive: true};
+let player = {
+    age: 99,
+    name: 'ABC',
+    isActive: true,
+    outfit: {
+        color: 'blue',
+        size: 'M',
+        cost: 100, 
+        isNew: true,
+    }
+};
 
 console.log(player);
 
 console.log(player.name);
-console.log(player.['name']);
-
-player .isActive = false;
-console.log(player);
+console.log(player['name']);
 
 player.health = 100;
 console.log(player);
 
-delete player.health
-console.log(player)
+delete player.health;
+console.log(player);
+
+console.log(player.outfit.color)
+
+player.outfit.size = 'S';
+console.log(player);
+
+
+let player = {
+    health: 100,
+    fun: 0,
+    eatApple: function() {
+        console.log('eat apple');
+
+        //this.health = this.health + 10
+        this.health += 10;
+
+        console.log(this.health);
+    };
+}
+
+player.eatApple();
+console.log(player);
