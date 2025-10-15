5c2c79bb1bc606227cbfc01bb8e9e99c200b0bf2
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-10-15 18:02:58 +1000

    601

---
+++ JoshuaH/intro_to_js/javascript/27-44file.js
@@ -0,0 +1,14 @@
+function sendSignal(){
+    console.log('Help')
+}
+
+/* we havent run this 1000 times */
+while(i < 1000){
+    sendSignal();
+    //i = i+1;
+    //i += 1;
+    i++;
+}
+
+let result = 0;
+
--- JoshuaH/intro_to_js/javascript/ja.js
+++ JoshuaH/intro_to_js/javascript/ja.js
@@ -27,18 +27,31 @@ player.outfit.size = 'S';
 console.log(player);
 
 
+// implement methods for player object
 let player = {
-    health: 100,
-    fun: 0,
-    eatApple: function() {
-        console.log('eat apple');
-
-        //this.health = this.health + 10
-        this.health += 10;
-
-        console.log(this.health);
+  health: 100,
+  fun: 0,
+  play: function() {
+    this.fun += 10;
+  },
+  eat: function(food) {
+    if(food == 'apple') {
+      this.health += 10;
     }
+    else if(food == 'candy') {
+      this.health -= 5;
+      this.fun += 5;
+    }
+  }
+  
 };
+
+// execute methods
+player.play();
+console.log(player);
+
+player.eat('apple');
+console.log(player);
 
 player.eatApple();
 console.log(player);
