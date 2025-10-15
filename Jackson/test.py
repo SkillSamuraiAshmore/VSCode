7550fbaa931a7f002be981d0e51c9286593c84bb
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-10-15 16:40:13 +1000

    math functions

--- Jackson/test.py
+++ Jackson/test.py
@@ -1,3 +1,6 @@
+from math import *
+name = input("enter your name")
+print("hello" + name + "!")
 print("that was fun! lets draw a right angle triangle")
 print("   /|")
 print("  / |")
@@ -5,14 +8,215 @@ print(" /  |")
 print("/___|")
 
 print("varible")
-character_name="ginger bread man"
-character_age="1000"
+character_name="steve"
+character_age="1,000,000,000,000,000"
 is_male = True
 print("There was once a man named, " + character_name +",")
 print("he was " + character_age + " years old. ") 
   
-print("strings")
-print("skill\n samurai") 
+print(3*(4+5))
+print(10/3) 
+
+
+my_num = -67
+
+print(str(my_num) + " my favirite number")
+
+print(abs(my_num))
+
+print(pow(4, 6))
+
+print(max(4, 6))
+
+print(min(4, 6))
+
+print(round(3.46568374873))
+
+
+
+
+print(floor(3.7))
+
+print(ceil(3.7))
+
+print(sqrt(25))
+
+
+
+
+print("   _________________________________________________________________________")
+print("  /                                                                         \   ") 
+print("_/___________________________________________________________________________\_")
+print("|        _______                 _______                 _______             |")
+print("|        |     |                 |     |                 |     |             |")
+print("|        |_____|                 |_____|                 |_____|             |")
+print("|____________________________________________________________________________|")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
   
