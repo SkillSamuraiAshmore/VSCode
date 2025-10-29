974c297551a42548e7828940d911d7577ec5f83f
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-10-29 17:49:05 +1000

    a

--- VanM/app.py
+++ VanM/app.py
@@ -1,14 +1,15 @@
+from pyexpat.errors import messages
 from openai import OpenAI
 
 # pass the API key
 client = OpenAI()
 # define prompt
-message = {role: 'user', 'content':, 'why is my website now?'}
-messages = []
-message.append(message)
+messages = {"role": 'user', "content":'why is my website now?'}
+messages.append({"role": 'system', "content":'why is my website now?'})
+messages.append({"role": 'user', "content":'why is my website now?'})
 
 # make and api
 response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages)
 
 # print the response
-print(response)
+print(response.choices[0].mess)
