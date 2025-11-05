6e6aa9ad5badf7eb6ad06fa8271c5e9413bbd5e0
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-11-05 17:57:12 +1000

    parameters

--- VanM/app.py
+++ VanM/app.py
@@ -2,14 +2,17 @@ from pyexpat.errors import messages
 from openai import OpenAI
 
 # pass the API key
-client = OpenAI()
+client = OpenAI(
+    api_key = ''   
+)
 # define prompt
-messages = {"role": 'user', "content":'why is my website now?'}
+
+messages = []
 messages.append({"role": 'system', "content":'why is my website now?'})
 messages.append({"role": 'user', "content":'why is my website now?'})
 
 # make and api
-response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages)
+response = client.chat.completions.create(messages=messages, model="gpt-4.1", n=2, max_tokens=100, temperature=0)
 
 # print the response
-print(response.choices[0].mess)
+print(response.choices[0].message.content)
