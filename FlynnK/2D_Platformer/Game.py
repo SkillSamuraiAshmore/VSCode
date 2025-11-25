a5943ed058a6a05c4e8ccb777484c2edd5b69fa4
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-11-25 16:57:08 +1000

    Added playeer class

--- FlynnK/2D_Platformer/Game.py
+++ FlynnK/2D_Platformer/Game.py
@@ -8,12 +8,23 @@ pygame.init()
 
 pygame.display.set_caption("Platformer with no name")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
@@ -22,14 +33,16 @@ def get_background(name):
     
     for i in range(WIDTH // width + 1):
         for j in range(HEIGHT // height + 1):
-            pos = [i * width, j * height]
+            pos = (i * width, j * height)
             tiles.append(pos)
             
     return tiles, image
 
 def draw(window, background, bg_image):
     for tile in background:
-        window.blit(bg_image, tuple(tile))
+        window.blit(bg_image, tile)
+        
+    pygame.display.update()
 
 def main(window):
     clock = pygame.time.Clock()
@@ -43,6 +56,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+            
+        draw(window, background, bg_image)
 
     pygame.quit()
     quit()
