a3a58d79b1e2952b71139ed6b8f7a75818e15556
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2025-12-02 16:34:52 +1000

    yep

--- FlynnK/2D_Platformer/Game.py
+++ FlynnK/2D_Platformer/Game.py
@@ -14,17 +14,47 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+    return [pygame.transform. flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False)
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 1
     
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+        
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
+        
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+    
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+            
+    def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+        
+        self.fall_count += 1
+        
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
@@ -38,15 +68,29 @@ def get_background(name):
             
     return tiles, image
 
-def draw(window, background, bg_image):
+def draw(window, background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
         
+    player.draw(window)
+        
     pygame.display.update()
+    
+    
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+    
+    player.x_vel = 0
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        player.move_right(PLAYER_VEL)
 
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
+    
+    player = Player(100,100, 50, 50)
     
     run = True
     while run:
@@ -57,7 +101,9 @@ def main(window):
                 run = False
                 break
             
-        draw(window, background, bg_image)
+        player.loop(FPS) 
+        handle_move(player)
+        draw(window, background, bg_image, player)
 
     pygame.quit()
     quit()
