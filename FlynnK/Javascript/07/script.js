15f2b15757455f35fc5da05b88ef9e384f3f27a0
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2026-02-19 17:03:21 +1000

    js

---
+++ FlynnK/Javascript/07/script.js
@@ -0,0 +1,27 @@
+//function hourToMinutes(hours) {
+//  let result = hours * 60;
+//  console.log(result);
+//  return result;
+//}
+
+//let a = hourToMinutes(10);
+//let b = hourToMinutes(20);
+//console.log(a);
+//console.log(b);
+
+//let dayToHours = function(days) {
+//  return days * 24;
+//}
+
+//let c = dayToHours(7);
+//console.log(c);
+
+let balance = 100;
+let stock = 50;
+let price = 5;
+let quantity = 8;
+
+if(stock >= 1) {
+  stock - 1;
+  
+}
