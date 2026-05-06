47101553331ac28f97f7a6cb2d74a151cb14666c
SkillSamuraiAshmore <160701247+SkillSamuraiAshmore@users.noreply.github.com>
2026-05-06 17:59:11 +1000

    added images

--- VanM/Food_Game_Project/game.py
+++ VanM/Food_Game_Project/game.py
@@ -2,16 +2,37 @@ import pygame
 import sys
 
 pygame.init()
+
+SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 600
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("FRIED")
 runwin = True
+
+def cursor():
+    # Get the rectangle of the image
+    image = pygame.image.load("assets\images\Open_Palm_Hand.png")
+    image_rect = image.get_rect()
+
+    image_rect.topleft = (100, 150) # Example position
+    
+    #need to use blit to get image on screen
+
+# Main game loop!
 while runwin:
    for event in pygame.event.get():
+       
+        cursor()
         if event.type == pygame.QUIT:
             runwin = False
             pygame.display.update()
+            
             window.fill((0, 0, 0))
-pygame.quit()
+            pygame.quit()   
+            
+
+
+
 
   
   
